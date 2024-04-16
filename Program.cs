b961b70cd9e789a799fa37ffaf8b80c08d187636
AndreaDev237 <ps2invader@yahoo.it>
2024-04-16 16:31:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_cibi_preferiti_new
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
